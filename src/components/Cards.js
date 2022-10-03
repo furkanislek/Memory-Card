cde3fbdefad0ea37be31d4aca800d9a631bea63e
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-10-03 19:43:12 +0300

    img

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,8 +3,32 @@ import {useState} from 'react'
 
 
 function Cards() {
+
+    const [items, setItems] = useState([
+        {id: 1, img: '/img/adana.jpg', stat: ""},
+        {id: 1, img: '/img/adana.jpg', stat: ""},
+        {id: 2, img: '/img/balcan.jpg', stat: ""},
+        {id: 2, img: '/img/balcan.jpg', stat: ""},
+        {id: 3, img: '/img/beyti.jpg', stat: ""},
+        {id: 3, img: '/img/beyti.jpg', stat: ""},
+        {id: 4, img: '/img/cızbız.jpg', stat: ""},
+        {id: 4, img: '/img/cızbız.jpg', stat: ""},
+        {id: 5, img: '/img/inegöl.jpg', stat: ""},
+        {id: 5, img: '/img/inegöl.jpg', stat: ""},
+        {id: 6, img: '/img/iskender.jpg', stat: ""},
+        {id: 6, img: '/img/iskender.jpg', stat: ""},
+        {id: 7, img: '/img/tavuk.jpg', stat: ""},
+        {id: 7, img: '/img/tavuk.jpg', stat: ""},
+        {id: 8, img: '/img/tavukdöner.jpg', stat: ""},
+        {id: 8, img: '/img/tavukdöner.jpg', stat: ""}
+    ])
+
   return (
-    <div>Cards</div>
+    <div className='container'>
+        {items.map(item => (
+            
+        ))}
+    </div>
   )
 }
 
