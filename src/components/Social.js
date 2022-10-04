404f87aa2bbaf8ea7d32ed478285d986d41d7ee7
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-10-04 15:58:28 +0300

    finish

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Social() {
   return (
-    <div class="gitIcon">
+    <div className="gitIcon">
         <a href="https://github.com/furkanislek" rel='noreferrer' target="_blank" className='icons'  >
-            <i class="fa-brands fa-github " ></i>
+            <i className="fa-brands fa-github " ></i>
         </a>
         <a href="https://www.linkedin.com/in/furkanislek/" rel='noreferrer' target="_blank" className='icons' >
-        <i class="fa-brands fa-linkedin"></i>
+        <i className="fa-brands fa-linkedin"></i>
         </a>
     </div>
 
