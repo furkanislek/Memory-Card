037128ace75f356f925681f0da7f7e0890f62420
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-10-04 15:20:24 +0300

    2

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function Card({item}) {
+function Card({item, id, handleClick}) {
+
+    const itemClass = item.stat ? " active " + item.stat : ""
+
   return (
-    <div className='card'>
+    <div className={"card" + itemClass } onClick={() => handleClick(id)} >
         <img src={item.img} alt=""/> 
     </div>
   )
